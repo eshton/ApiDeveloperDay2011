027be229e64b921f84e05d2712c0ccc1b6a8962d
Agoston Fung <agoston.fung@gmail.com>
2011-10-01 15:47:58 +0100

    services

---
+++ services/sms.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-type: application/json');
+
+$msisdn = $_GET['msisdn'];
+$message = $_GET['message'];
+
+if (!$msisdn || !$message) {
+    echo json_encode(array('result' => 'wrong parameter'));
+    return;
+}
+
+include '../lib/BUTESmsSender.php';
+
+$test = new BUTESmsSender(false);
+$test->send($msisdn, $message);
+
+echo json_encode(array('result' => 'success'));
